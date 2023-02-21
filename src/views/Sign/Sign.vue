b3977cfa1b39c51e5a361a34c3b0dcff2216d8fb
Mussued <skossm@outlook.com>
2023-02-21 14:13:21 +0800

    月份控件联动描述列表和日历

--- src/views/Sign/Sign.vue
+++ src/views/Sign/Sign.vue
@@ -1,12 +1,13 @@
 <template>
   <el-descriptions border direction="vertical" :column="9">
     <el-descriptions-item label="月份">{{ month }}月</el-descriptions-item>
-    <el-descriptions-item label="正常出勤">0</el-descriptions-item>
+    <el-descriptions-item v-for="value, key in DetailKey" :key="key" :label="value">{{ detailValue[key]
+    }}</el-descriptions-item>
     <el-descriptions-item label="操作">
-      <el-button type="primary" plain size="small">查看详情</el-button>
+      <el-button type="primary" plain size="small" @click="handleToException">查看详情</el-button>
     </el-descriptions-item>
     <el-descriptions-item label="考勤状态">
-      <el-tag type="danger" size="small">异常</el-tag>
+      <el-tag :type="detailState.type" size="small">{{ detailState.text }}</el-tag>
     </el-descriptions-item>
   </el-descriptions>
   <el-calendar v-model="date">
@@ -14,7 +15,7 @@
       <el-button type="primary">在线签到</el-button>
       <el-space>
         <el-button plain>{{ year }}年</el-button>
-        <el-select v-model="month">
+        <el-select v-model="month" @change="handleChange">
           <el-option v-for="item in 12" :key="item" :value="item" :label="item + '月'" />
         </el-select>
       </el-space>
@@ -23,12 +24,45 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
 
 const date = ref(new Date())
 const year = date.value.getFullYear()
 const month = ref(date.value.getMonth() + 1)
 
+enum DetailKey {
+  normal = '正常出勤',
+  absent = '旷工',
+  miss = '漏打卡',
+  late = '迟到',
+  early = '早退',
+  lateAndEarly = '迟到并早退'
+}
+
+const detailValue = reactive({
+  normal: 0,
+  absent: 0,
+  miss: 0,
+  late: 0,
+  early: 0,
+  lateAndEarly: 0
+})
+
+const detailState = reactive({
+  type: 'success' as 'success' | 'danger',
+  text: '正常' as '正常' | '异常'
+})
+
+const handleChange = () => {
+  date.value = new Date(`${year}.${month.value}`)
+}
+
+const handleToException = () => {
+  router.push('/exception')
+}
 </script>
 
 <style scoped lang="scss">
