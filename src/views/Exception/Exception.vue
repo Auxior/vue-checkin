c0f270e52292a39856b58837bf3e32088c26f0a1
Mussued <skossm@outlook.com>
2023-02-21 17:19:19 +0800

    时间线数据渲染与交互功能实现

--- src/views/Exception/Exception.vue
+++ src/views/Exception/Exception.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="exception-title">
-    <el-button type="primary">异常处理</el-button>
+    <el-button type="primary" @click="handleToApply">异常处理</el-button>
     <el-space>
       <el-button plain>{{ year }}年</el-button>
       <el-select v-model="month">
@@ -12,19 +12,12 @@
     <el-col :span="12">
       <el-empty v-if="false" description="暂无异常考勤" />
       <el-timeline v-else>
-        <el-timeline-item timestamp="2023/2/21" placement="top">
+        <el-timeline-item v-for="item in detailMonth" :key="item[0]" :timestamp="year + '/' + month + '/' + item[0]"
+          placement="top">
           <el-card>
             <el-space>
-              <h4>旷工</h4>
-              <p>考勤详情：暂无打卡记录</p>
-            </el-space>
-          </el-card>
-        </el-timeline-item>
-        <el-timeline-item timestamp="2023/2/21" placement="top">
-          <el-card>
-            <el-space>
-              <h4>旷工</h4>
-              <p>考勤详情：暂无打卡记录</p>
+              <h4>{{ item[1] }}</h4>
+              <p>考勤详情：{{ renderTime(item[0]) }}</p>
             </el-space>
           </el-card>
         </el-timeline-item>
@@ -54,21 +47,43 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import { ref, watch, computed } from 'vue'
 import { useRoute, useRouter } from 'vue-router';
+import { useStore } from '@/store';
+import { toZero } from '@/utils/common';
 
 const route = useRoute()
 const router = useRouter()
+const store = useStore()
+
+const signsInfos = computed(() => store.state.signs.infos)
 
 const date = new Date()
 const year = date.getFullYear()
 const month = ref(Number(route.query.month) || date.getMonth() + 1)
+
+const ret = (signsInfos.value.detail as { [index: string]: unknown })[toZero(month.value)] as { [index: string]: unknown }
+
+const detailMonth = computed(() => Object.entries(ret).filter((v) => v[1] !== '正常出勤').sort())
 
 watch(month, () => {
   router.push({
     query: { month: month.value }
   })
 })
+
+const handleToApply = () => {
+  router.push('/apply')
+}
+
+const renderTime = (date: string) => {
+  const ret = ((signsInfos.value.time as { [index: string]: unknown })[toZero(month.value)] as { [index: string]: unknown })[date];
+  if (Array.isArray(ret)) {
+    return ret.join('-')
+  } else {
+    return '暂无打卡记录'
+  }
+}
 </script>
 
 <style scoped lang="scss">
