f26e2815cec89c81eb7066dd86b5a73c15536b34
Mussued <skossm@outlook.com>
2023-02-21 15:49:05 +0800

    描述列表数据渲染与签到联动

--- src/views/Sign/Sign.vue
+++ src/views/Sign/Sign.vue
@@ -28,10 +28,11 @@
 </template>
 
 <script setup lang="ts">
-import { ref, reactive, computed } from 'vue';
+import { ref, reactive, computed, watchEffect } from 'vue';
 import { useRouter } from 'vue-router';
 import { useStore } from '@/store';
 import { ElMessage } from 'element-plus';
+import { toZero } from '@/utils/common'
 
 const router = useRouter()
 const store = useStore()
@@ -64,6 +65,49 @@ const detailValue = reactive({
 const detailState = reactive({
   type: 'success' as 'success' | 'danger',
   text: '正常' as '正常' | '异常'
+})
+
+watchEffect((reset) => {
+  const detailMonth = ((signsInfos.value.detail as { [index: string]: unknown })[toZero(month.value)] as { [index: string]: unknown })
+
+  for (const attr in detailMonth) {
+    switch (detailMonth[attr]) {
+      case DetailKey.normal:
+        detailValue.normal++
+        break
+      case DetailKey.absent:
+        detailValue.absent++
+        break
+      case DetailKey.miss:
+        detailValue.miss++
+        break
+      case DetailKey.late:
+        detailValue.late++
+        break
+      case DetailKey.early:
+        detailValue.early++
+        break
+      case DetailKey.lateAndEarly:
+        detailValue.lateAndEarly++
+        break
+    }
+  }
+
+  for (const attr in detailValue) {
+    if (attr !== 'normal' && detailValue[attr as keyof typeof detailValue] !== 0) {
+      detailState.type = 'danger'
+      detailState.text = '异常'
+    }
+  }
+
+  reset(() => {
+    detailState.type = 'success'
+    detailState.text = '正常'
+
+    for (const attr in detailValue) {
+      detailValue[attr as keyof typeof detailValue] = 0
+    }
+  })
 })
 
 const handleChange = () => {
